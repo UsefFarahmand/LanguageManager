15b056248de7e9d390c8737473ab5363e9922e18
UsefFarahmand <50228817+UsefFarahmand@users.noreply.github.com>
2023-04-19 01:16:41 +0430

    feat create DictionaryAsset  scriptable object

---
+++ Assets/DiaGna/LanguageManager/Scripts/ScriptableObjects/DictionaryAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DiaGna.Freamwork.Language
+{
+    [CreateAssetMenu(fileName = "DictionaryAsset", menuName = "DiaGna/Freamwork/Language/DictionaryAsset")]
+    public class DictionaryAsset : ScriptableObject
+    {
+        [SerializeField] private Dictionary m_Dictionary;
+
+        public Dictionary Dictionary => m_Dictionary;
+    }
+}
