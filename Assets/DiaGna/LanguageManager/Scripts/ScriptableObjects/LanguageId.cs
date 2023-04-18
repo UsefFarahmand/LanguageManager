811248a7c682e3774bea953ae4255665878b5dcb
UsefFarahmand <50228817+UsefFarahmand@users.noreply.github.com>
2023-04-19 01:16:38 +0430

    feat create LanguageId scriptable object

---
+++ Assets/DiaGna/LanguageManager/Scripts/ScriptableObjects/LanguageId.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DiaGna.Freamwork.Language
+{
+    /// <summary>
+    /// Represents a language identifier as a ScriptableObject.
+    /// </summary>
+    [CreateAssetMenu(fileName = "LanguageId", menuName = "DiaGna/Freamwork/Language/LanguageId")]
+    public class LanguageId : ScriptableObject
+    {
+        public string Id => name;
+    }
+}
